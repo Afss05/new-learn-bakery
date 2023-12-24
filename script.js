d19ecb96bd5ed480cad6de928f16eedb4e6a4c5f
Afss05 <sheikabu1605@gmail.com>
2023-12-24 17:36:02 +0530

    well come learn to github

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function signup() {
+    document.getElementById("up").style.display="block";
+    document.getElementById("in").style.display="none";
+}
+
+function profile(){
+    document.getElementById("applicate").style.display="block";
+    document.getElementById("bg").style.display="block";
+}
+
+
+ 
